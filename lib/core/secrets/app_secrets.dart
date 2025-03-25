7ae59ca99cbd92931cf46a6fea0cf7391710af45
_@6h057_7yp3 <ngopsyjr2@gmail.com>
2025-03-25 07:19:06 +0200

    debug commit

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://ntqlcvrcxjwxxhtkgoly.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50cWxjdnJjeGp3eHhodGtnb2x5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4MzIwNzEsImV4cCI6MjA1ODQwODA3MX0.vN7XRapPpLnMMlXtN7zNyY3gKuYEcXz6ffKHm56Z1ok';
+}
