7b5c10289705bb647d3fb211bcf41a0a874e56f7
_@6h057_7yp3 <ngopsyjr2@gmail.com>
2025-03-25 09:24:49 +0200

    auth bloc def

--- lib/features/auth/data/datasource/auth_remote_data_source.dart
+++ lib/features/auth/data/datasource/auth_remote_data_source.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/core/error/exceptions.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class AuthRemoteDataSource {
@@ -17,16 +18,33 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   AuthRemoteDataSourceImpl(this.supabaseClient);
 
   @override
-  Future<String> loginWithEmailAndPassword(
-      {required String email, required String password}) {
-    // TODO: implement loginWithEmailAndPassword
+  Future<String> loginWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
     throw UnimplementedError();
   }
 
   @override
-  Future<String> signUpWithEmailAndPassword(
-      {required String name, required String email, required String password}) {
-    // TODO: implement signUpWithEmailAndPassword
-    throw UnimplementedError();
+  Future<String> signUpWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        password: password,
+        email: email,
+        data: {
+          'name': name,
+        },
+      );
+      if (response.user == null) {
+        throw const ServerException("User is null!");
+      }
+      return response.user!.id;
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
   }
 }
